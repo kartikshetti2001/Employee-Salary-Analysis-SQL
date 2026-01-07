ee17a84d38853d399436d3fb3db1604bbed6de18
Kartik Shetti <kshetti97@gmail.com>
2026-01-07 11:27:00 +0530

    Create departments and employees tables

---
+++ tables.sql
@@ -0,0 +1,13 @@
+CREATE TABLE departments (
+    dept_id INT PRIMARY KEY,
+    dept_name VARCHAR(50)
+);
+
+CREATE TABLE employees (
+    emp_id INT PRIMARY KEY,
+    emp_name VARCHAR(50),
+    dept_id INT,
+    salary INT,
+    join_date DATE,
+    FOREIGN KEY (dept_id) REFERENCES departments(dept_id)
+);
